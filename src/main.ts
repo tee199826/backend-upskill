39f551ff90a49ff3ef7069849fbb95712a205671
siwapan <work.siwapun.kh@gmail.com>
2025-05-27 21:02:48 +0700

    Refactor code structure and remove redundant sections for improved readability and maintainability

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+
+  // ตั้งค่า Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Insure Group API')
+    .setDescription(
+      'API documentation with JWT authentication for Upskilling and Reskilling programs',
+    )
+    .setVersion('1.0')
+    .addBearerAuth() // เพิ่มการรองรับ Bearer Token สำหรับ JWT
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
